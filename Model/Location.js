95af21116d901da34931dd9e9c7f658c5881e947
asomaniKofi <31180345+asomaniKofi@users.noreply.github.com>
2025-07-12 14:46:11 +0100

    Converted to TS

--- Model/Location.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const locationSchema = new mongoose.Schema({
-    Name:String,
-    Areas: []
-},{ collection : 'Locations'});
-
-const Location = mongoose.model("Locations", locationSchema);
-module.exports = Location;
